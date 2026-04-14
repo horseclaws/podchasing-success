889fd0970c437bac2a3ed94bf495c15a9eb3b84f
horseclaws <jon.dispenza@podchaser.com>
2026-04-14 17:27:26 -0400

    Set basePath to /percy for podchaser.ai deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: process.env.NODE_ENV === 'production' ? '/percy' : '',
 };
 
 export default nextConfig;
